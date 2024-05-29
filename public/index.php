031ec649c0283de707aba772bb7628fdc3075c4b
George Repiev <repev.egor@nttek.ru>
2024-05-29 09:12:41 +0500

    Init an empty repo...

---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+
+use App\Ega22a\InitClass;
+
+print(InitClass::$hello_world);
---
+++ src/Ega22a.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Ega22a;
+
+class InitClass {
+    static $hello_world = 'Hello, World!';
+}
